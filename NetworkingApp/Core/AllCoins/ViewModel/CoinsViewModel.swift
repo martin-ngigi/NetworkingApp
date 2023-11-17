b6a79592030579d8b26ab0f8ab88081cfebe527b
martin-ngigi <martinwainaina001@gmail.com>
2023-11-17 14:25:49 +0300

    Error handling

--- NetworkingApp/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingApp/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class CoinsViewModel: ObservableObject{
     @Published var coin = ""
     @Published var price = ""
+    @Published var errorMessage : String?
     
     init() {
         fetchCoin(coin: "litecoin")
@@ -19,34 +20,51 @@ class CoinsViewModel: ObservableObject{
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         
-        print("---> Fetching price")
         URLSession.shared.dataTask(with: url) { data, response, error in
-            print("---> Did Receive data: \(data)")
-            guard let data = data else { return }
-            /// Convert to Json object
-            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                print("*** Error: Failed to pass jsonObject")
-                return
-            }
-            print("---> JSON: \(jsonObject)") /// in jsonObject remove as? [String: Any] which is type casting
-            
-            guard let value = jsonObject["\(coin)"] as? [String: Double] else {
-                print("*** Error: Failed to pass value")
-                return
-            }
-            guard let price = value["usd"] else {
-                print("*** Error: Failed to pass price")
-                return
-            }
-            
             DispatchQueue.main.async {
+                if let error = error{
+                    print("*** Error: \(error.localizedDescription)")
+                    self.errorMessage = error.localizedDescription
+                    return
+                }
+                //print("---> Did Receive data: \(data!)")
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    self.errorMessage = "Error: Bad Http response"
+                    print("*** Error: Bad Http response")
+                    return
+                }
+                
+                guard httpResponse.statusCode == 200 else{
+                    self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode) "
+                    return
+                }
+                
+                guard let data = data else {
+                    print("*** Error: Failed to pass data")
+                    return
+                }
+                /// Convert to Json object
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("*** Error: Failed to pass jsonObject")
+                    return
+                }
+                print("---> JSON: \(jsonObject)") /// in jsonObject remove as? [String: Any] which is type casting
+                
+                guard let value = jsonObject["\(coin)"] as? [String: Double] else {
+                    print("*** Error: Failed to pass value")
+                    return
+                }
+                guard let price = value["usd"] else {
+                    print("*** Error: Failed to pass price")
+                    return
+                }
+                
+            
                 self.coin = coin.capitalized
                 self.price = "\(price)"
             }
         }.resume()
         
-        print("---> Did reach end of function.")
-
     }
     
 }
