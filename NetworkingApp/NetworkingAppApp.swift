74882c70c45719fd5bf869902b9cc7bd75611235
martin-ngigi <martinwainaina001@gmail.com>
2023-11-17 08:36:24 +0300

    Initial commit

---
+++ NetworkingApp/NetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAppApp.swift
+//  NetworkingApp
+//
+//  Created by Martin Wainaina on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
