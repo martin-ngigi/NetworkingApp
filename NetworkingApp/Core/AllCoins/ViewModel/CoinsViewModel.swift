6ae611b39babe8d0d164ea3a67d7d41099006c67
martin-ngigi <martinwainaina001@gmail.com>
2023-11-17 12:49:03 +0300

    Parse Json data

--- NetworkingApp/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingApp/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -12,16 +12,41 @@ class CoinsViewModel: ObservableObject{
     @Published var price = ""
     
     init() {
-        fetchCoin()
+        fetchCoin(coin: "litecoin")
     }
     
-    func fetchCoin(){
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=bitocoin&vs_currencies=usd"
+    func fetchCoin(coin: String){
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         
+        print("---> Fetching price")
         URLSession.shared.dataTask(with: url) { data, response, error in
-            print("------> Did Receive data: \(data)")
+            print("---> Did Receive data: \(data)")
+            guard let data = data else { return }
+            /// Convert to Json object
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                print("*** Error: Failed to pass jsonObject")
+                return
+            }
+            print("---> JSON: \(jsonObject)") /// in jsonObject remove as? [String: Any] which is type casting
+            
+            guard let value = jsonObject["\(coin)"] as? [String: Double] else {
+                print("*** Error: Failed to pass value")
+                return
+            }
+            guard let price = value["usd"] else {
+                print("*** Error: Failed to pass price")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "\(price)"
+            }
         }.resume()
+        
+        print("---> Did reach end of function.")
+
     }
     
 }
